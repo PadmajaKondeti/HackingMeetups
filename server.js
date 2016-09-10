af39e1b37f5c14c38094f53f7760311affcb8b08
padmajakondeti <padma.kondeti@gmail.com>
2016-09-10 14:56:58 -0400

    changed port

--- server.js
+++ server.js
@@ -193,6 +193,6 @@ app.post('/delete/:id', function(req, res){
 });
 
 // listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('App running on port 3000!');
 });
